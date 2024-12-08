101c21803855105b6f5d3e681e01acb905c66faf
ATQUIL <creator.atquil@gmail.com>
2024-12-08 13:36:24 +0530

    first commit

---
+++ src/main/java/com/atquil/springbootfaulttolerance/SpringBootFaultToleranceApplication.java
@@ -0,0 +1,13 @@
+package com.atquil.springbootfaulttolerance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFaultToleranceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFaultToleranceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atquil/springbootfaulttolerance/SpringBootFaultToleranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atquil.springbootfaulttolerance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFaultToleranceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
